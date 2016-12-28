04b9babadb73cea62e47b98113e712e9d0cfa943
J0hn- <jonathan.platteau@yahoo.fr>
2016-12-28 10:07:57 +0100

    Fix name problem on install script

--- install_and_run.bat
+++ install_and_run.bat
@@ -1,5 +1,5 @@
 docker-compose down
 docker-compose up -d
-docker exec symfony_symfony_1 php bin/console doctrine:database:create
-docker exec symfony_symfony_1 php bin/console doctrine:schema:update --complete --force
-docker exec symfony_symfony_1 php bin/console doctrine:fixtures:load -n
+docker exec polytechcinemaserver_symfony_1 php bin/console doctrine:database:create
+docker exec polytechcinemaserver_symfony_1 php bin/console doctrine:schema:update --complete --force
+docker exec polytechcinemaserver_symfony_1 php bin/console doctrine:fixtures:load -n
