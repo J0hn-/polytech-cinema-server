fa4007cee822d7b2dffaeded3e61d6ca82e39cd5
J0hn <jonathan.platteau@yahoo.fr>
2016-12-29 15:48:43 +0100

    Fixing rights and update docker-compose (#5)
    Fixing rights problems

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 docker-compose down
 docker-compose up -d
-docker exec -it polytechcinemaserver_symfony_1 bash
+docker exec -it cinema_symfony bash
 php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --complete --force
 php bin/console doctrine:fixtures:load -n
+chmod -R o+w var
 exit
